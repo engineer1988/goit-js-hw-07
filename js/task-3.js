8a3980eeafa078258df111a1264cdb95a74cfc29
Dmytro Maniuta <ip722428@gmail.com>
2023-12-31 19:27:36 +0200

    finished task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,8 +2,11 @@ const textInput = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 textInput.addEventListener('input', event => {
-  output.textContent = event.currentTarget.value;
-  if (output.textContent === '' || output.textContent === ' ') {
+  output.textContent = event.currentTarget.value.trim();
+  if (
+    event.currentTarget.value.trim() === '' ||
+    event.currentTarget.value.trim() === ' '
+  ) {
     output.textContent = 'Anonymous';
   }
 });
