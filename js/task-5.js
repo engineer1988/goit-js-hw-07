e7316514b88e13ff3fd034e91474511bea170555
Dmytro Maniuta <ip722428@gmail.com>
2024-01-01 17:46:36 +0200

    finished task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const widget = document.querySelector('body');
+const color = document.querySelector('.color');
+const changeColor = document.querySelector('.change-color');
+changeColor.addEventListener('click', getRandoHmexColor);
+
+function getRandoHmexColor() {
+  return (widget.style.backgroundColor = color.textContent =
+    `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`);
 }
