5778a89901f295bcf0c1e080715bbcbcecc2f3c1
Dmytro Maniuta <ip722428@gmail.com>
2024-01-06 19:45:20 +0200

    hw-7

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const numberInput = document.querySelector('input');
+const createFocusBtn = document.querySelector('[data-create]');
+const destroyFocusBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+createFocusBtn.addEventListener('click', createBoxes);
+
+function createBoxes() {
+  let i = 0;
+  let b = 30;
+  while (i < numberInput.value) {
+    const item = document.createElement('div');
+    item.style.width = b + 'px';
+    item.style.height = b + 'px';
+    item.style.backgroundColor = getRandomHexColor();
+    boxes.append(item);
+    i++;
+    b += 10;
+  }
+}
+
+destroyFocusBtn.addEventListener('click', () => {
+  numberInput.value = '';
+});
